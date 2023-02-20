75e89cbb914707982b366aa60524ea7625dabc61
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-02-20 11:40:40 +0700

    integrating firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDDOji2Mdxh-KmdljEKQBXH4i1R8_aGDvU",
   authDomain: "linkedin-clone-f345d.firebaseapp.com",
@@ -6,3 +8,9 @@ const firebaseConfig = {
   messagingSenderId: "668268392630",
   appId: "1:668268392630:web:9d3fb805e274da205ff617",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
