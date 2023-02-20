2eabe16b17006cebcb551cde7bbcd22bcaafb5ba
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-02-20 10:42:38 +0700

    integrating firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDDOji2Mdxh-KmdljEKQBXH4i1R8_aGDvU",
+  authDomain: "linkedin-clone-f345d.firebaseapp.com",
+  projectId: "linkedin-clone-f345d",
+  storageBucket: "linkedin-clone-f345d.appspot.com",
+  messagingSenderId: "668268392630",
+  appId: "1:668268392630:web:9d3fb805e274da205ff617",
+};
