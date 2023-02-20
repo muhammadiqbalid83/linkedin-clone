de70969c2e4d49f13859547b7099fe845ef3c774
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-02-20 09:25:22 +0700

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      blue: "#0058a2",
+      yellow: "#e1bb00",
+      green: "#01ab5d",
+      "dark-text": "#1b1d1f",
+      "light-text": "#a9a8a8",
+      "icon-grey": "#adaeae",
+    },
     extend: {},
   },
   plugins: [],
